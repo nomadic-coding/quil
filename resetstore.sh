c139bd78e87e502b241336d314b8cac2aab890de
Kumar Aberer <hello@nomadic-coding.com>
2024-05-18 20:40:59 -0500

    Update resetstore.sh

--- resetstore.sh
+++ resetstore.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-# kill all tmux session
-pkill -f tmux
-
 # Function to check if a command exists
 command_exists() {
   command -v "$1" >/dev/null 2>&1
 }
+
+# Check if pip is installed
+if ! command_exists pip; then
+  echo "pip is not installed. Installing..."
+  sudo apt-get update
+  sudo apt-get -y install python3-pip
+  echo "pip installed successfully."
+else
+  echo "pip is already installed."
+fi
+
+# Check if gdown is installed
+if ! command_exists gdown; then
+  echo "gdown is not installed. Installing..."
+  pip install gdown
+  echo "gdown installed successfully."
+else
+  echo "gdown is already installed."
+fi
 
 # Check if unzip is installed
 if ! command_exists unzip; then
@@ -32,41 +48,32 @@ else
   echo "tmux is not installed."
 fi
 
-
-# Define the file path and the download URL
-REPAIR_FILE="/root/REPAIR"
-DOWNLOAD_URL="https://drive.google.com/uc?export=download&id=1_dciSwlcXFBjiWy9qiDlVPhpkllASNtT"
-
-# Check if the file exists
-if [ ! -f "$REPAIR_FILE" ]; then
-  echo "REPAIR file does not exist. Downloading..."
-  curl -L -o "$REPAIR_FILE" "$DOWNLOAD_URL"
-  echo "Download completed: $REPAIR_FILE"
-else
-  echo "REPAIR file already exists: $REPAIR_FILE"
-fi
-
-# Define the file path and the download URL
+# Define the STORE file and the directory
 STORE_FILE="/root/store368411.zip"
-DOWNLOAD_URL="https://drive.google.com/uc?export=download&id=1XPmG9vmG3VEH_ZE1wiyP2gBfW2zcMi5q"
+STORE_FILE_ID="1XPmG9vmG3VEH_ZE1wiyP2gBfW2zcMi5q"
+STORE_DIR="/root/store"
 
-# Check if the file exists
+# Function to download file from Google Drive using gdown
+download_from_google_drive() {
+  local file_id=$1
+  local destination=$2
+  gdown --id "$file_id" --output "$destination"
+}
+
+# Download the STORE file from Google Drive if it doesn't exist
 if [ ! -f "$STORE_FILE" ]; then
-  echo "STORE_FILE file does not exist. Downloading..."
-  curl -L -o "$STORE_FILE" "$DOWNLOAD_URL"
+  echo "STORE file does not exist. Downloading..."
+  download_from_google_drive "$STORE_FILE_ID" "$STORE_FILE"
   echo "Download completed: $STORE_FILE"
 else
-  echo "STORE_FILE file already exists: $STORE_FILE"
+  echo "STORE file already exists: $STORE_FILE"
 fi
-
-# Define the STORE file and the directory
-STORE_DIR="/root/store"
 
 # Check if the STORE directory exists and has files
 if [ -d "$STORE_DIR" ]; then
   if [ -z "$(ls -A $STORE_DIR)" ]; then
     echo "STORE directory is empty. Unzipping $STORE_FILE..."
-    unzip -j "$STORE_FILE" -d "$STORE_DIR"
+    unzip -o -j "$STORE_FILE" -d "$STORE_DIR"
     echo "Unzip completed: $STORE_FILE to $STORE_DIR"
   else
     echo "STORE directory is not empty."
@@ -74,13 +81,6 @@ if [ -d "$STORE_DIR" ]; then
 else
   echo "STORE directory does not exist. Creating and unzipping $STORE_FILE..."
   mkdir -p "$STORE_DIR"
-  unzip -j "$STORE_FILE" -d "$STORE_DIR"
+  unzip -o -j "$STORE_FILE" -d "$STORE_DIR"
   echo "Unzip completed: $STORE_FILE to $STORE_DIR"
 fi
-
-rm -rf /root/ceremonyclient/node/.config/store/*
-cd /root/
-cp -r /root/store/* /root/ceremonyclient/node/.config/store/
-cp /root/REPAIR /root/ceremonyclient/node/.config/REPAIR
-
-tmux new-session -d -s quil 'export PATH=$PATH:/usr/local/go/bin && cd /root/ceremonyclient/node && /root/scripts/qnode_restart.sh'
