12031dc5619e57e69dfdb21e55e8fb3f7d9712da
Kumar Aberer <hello@nomadic-coding.com>
2024-05-29 18:16:01 -0500

    Create updategitrepo.sh

---
+++ updategitrepo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Navigate to the repository directory
+cd /root/ceremonyclient/node || { echo "Directory not found"; exit 1; }
+
+echo "stop running service"
+kill -9 $(pgrep node-1.4.18) && service ceremonyclient stop
+
+# Stash any local changes
+git reset --hard
+
+# Set the new origin URL
+git remote set-url origin https://source.quilibrium.com/quilibrium/ceremonyclient.git
+
+# Fetch the branches from the new origin
+git fetch origin
+
+# Reset the local 'release' branch to match the remote 'release' branch
+git checkout release
+git reset --hard origin/release
+
+echo "Local repository reset and synced with the 'release' branch of the new remote successfully."
+
+echo "stop running start"
+service ceremonyclient start
