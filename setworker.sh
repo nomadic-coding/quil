b26205b534c2687832317d7c8e04cc0766022ea4
Kumar Aberer <hello@nomadic-coding.com>
2024-05-31 21:36:34 -0500

    Update setworker.sh

--- setworker.sh
+++ setworker.sh
@@ -6,27 +6,14 @@ sed -i '/^\[Install\]/,/^\[/{/Environment=GOMAXPROCS=2/d;}' /lib/systemd/system/
 # Get the number of CPU cores
 cores=$(nproc)
 
-# Get the total RAM in gigabytes using /proc/meminfo
-ram_kb=$(awk '/^MemTotal:/{print $2}' /proc/meminfo)
-ram=$((ram_kb / 1024 / 1024))  # Convert from KB to GB
+# Calculate GOMAXPROCS as 40% of the total cores
+gomaxprocs=$(awk "BEGIN {print int($cores * 0.4)}")
 
-# Calculate GOMAXPROCS
-# Set max_cores to 40% of the total cores or the number of potential GOMAXPROCS based on available RAM, whichever is lower
-required_ram_per_core=2
-max_cores_by_ram=$((ram / required_ram_per_core))
-max_cores_by_cores=$(awk "BEGIN {print int($cores * 0.5)}")
-max_cores=$((max_cores_by_ram < max_cores_by_cores ? max_cores_by_ram : max_cores_by_cores))
-
-# Ensure max_cores is at least 1
-max_cores=$((max_cores > 0 ? max_cores : 1))
-
-# Set GOMAXPROCS to the calculated max_cores
-gomaxprocs=$max_cores
+# Ensure GOMAXPROCS is at least 1
+gomaxprocs=$((gomaxprocs > 0 ? gomaxprocs : 1))
 
 # Print calculated values for debugging
 echo "Number of CPU cores: $cores"
-echo "Total RAM in GB: $ram"
-echo "Calculated max_cores: $max_cores"
 echo "Calculated GOMAXPROCS: $gomaxprocs"
 
 # Update or add Environment=GOMAXPROCS in the [Service] section of the service file
