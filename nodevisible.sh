a9741b2949d882964f59e1d838a21f6fdb2bd0f0
Kumar Aberer <hello@nomadic-coding.com>
2024-06-02 14:36:53 -0500

    Create nodevisible.sh

---
+++ nodevisible.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sleep 1
+
+# Bootstrap peer list
+bootstrap_peers=(
+"EiDpYbDwT2rZq70JNJposqAC+vVZ1t97pcHbK8kr5G4ZNA=="
+"EiCcVN/KauCidn0nNDbOAGMHRZ5psz/lthpbBeiTAUEfZQ=="
+"EiDhVHjQKgHfPDXJKWykeUflcXtOv6O2lvjbmUnRrbT2mw=="
+"EiDHhTNA0yf07ljH+gTn0YEk/edCF70gQqr7QsUr8RKbAA=="
+"EiAnwhEcyjsHiU6cDCjYJyk/1OVsh6ap7E3vDfJvefGigw=="
+"EiB75ZnHtAOxajH2hlk9wD1i9zVigrDKKqYcSMXBkKo4SA=="
+"EiDEYNo7GEfMhPBbUo+zFSGeDECB0RhG0GfAasdWp2TTTQ=="
+"EiCzMVQnCirB85ITj1x9JOEe4zjNnnFIlxuXj9m6kGq1SQ=="
+)
+
+# Run the grpcurl command and capture its output
+output=$(/root/go/bin/grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNetworkInfo)
+
+# Check if any of the specific peers are in the output
+visible=false
+for peer in "${bootstrap_peers[@]}"; do
+    if [[ $output == *"$peer"* ]]; then
+        visible=true
+        echo "You see $peer as a bootstrap peer"
+    else
+        echo "Peer $peer not found"
+    fi
+done
+echo $visible
