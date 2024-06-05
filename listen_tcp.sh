db9c06d34ac0b7a589ecf778bba81c196d0b63f0
Kumar Aberer <hello@nomadic-coding.com>
2024-06-04 21:46:14 -0500

    Create listen_tcp.sh

---
+++ listen_tcp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the file path
+CONFIG_FILE="/root/ceremonyclient/node/.config/config.yml"
+
+# Replace the listenMultiaddr line
+sed -i 's|listenMultiaddr: /ip4/0.0.0.0/udp/8336/quic|listenMultiaddr: /ip4/0.0.0.0/tcp/8336/quic|' "$CONFIG_FILE"
+
+echo "Replacement done in $CONFIG_FILE"
+
+ufw allow 8336/tcp
+
+echo "restarting service"
+service ceremonyclient restart
