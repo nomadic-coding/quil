961ba398875528243f91091967394b9433e841d0
Kumar Aberer <hello@nomadic-coding.com>
2024-06-06 12:11:42 -0500

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -10,7 +10,7 @@ kill -9 $(pgrep node-1.4.18) && service ceremonyclient stop
 wget --no-cache -O - https://raw.githubusercontent.com/lamat1111/quilibriumscripts/master/tools/qnode_gRPC_calls_setup.sh | bash
 kill -9 $(pgrep node-1.4.18) && service ceremonyclient stop
 go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
-apt-get y install jq
+apt-get -y install jq
 service ceremonyclient start
 echo "migrate done"
 
