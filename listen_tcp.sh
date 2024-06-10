6d7f12f54ebd761613eb02087cb38c163ff10ce1
Kumar Aberer <hello@nomadic-coding.com>
2024-06-10 12:34:15 -0500

    Update listen_tcp.sh

--- listen_tcp.sh
+++ listen_tcp.sh
@@ -4,7 +4,7 @@
 CONFIG_FILE="/root/ceremonyclient/node/.config/config.yml"
 
 # Replace the listenMultiaddr line
-sed -i 's|listenMultiaddr: /ip4/0.0.0.0/udp/8336/quic|listenMultiaddr: /ip4/0.0.0.0/tcp/8336|' "$CONFIG_FILE"
+sed -i 's|listenMultiaddr: /ip4/0.0.0.0/udp/8336/quic-v1|listenMultiaddr: /ip4/0.0.0.0/tcp/8336|' "$CONFIG_FILE"
 
 
 echo "Replacement done in $CONFIG_FILE"
